a0152d98946171061c0da47a3c3ce5e8e8b28718
Mohammad mehdi Daghighian <mohammadmehdidaghighian@gmail.com>
2026-09-12 12:39:25 +0330

    Update be_vaght_ahvaz_bot.py

--- be_vaght_ahvaz_bot.py
+++ be_vaght_ahvaz_bot.py
@@ -1,8 +1,8 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN ="8730995107:AAHeVio6hUQ-GGMxnPAKtacp-7A5pHKqnWs"
-ADMIN_ID = 218104646
+TOKEN ="your bot father token"
+ADMIN_ID = your ID
 
 ABOUT_BOT_TEXT = """سلام هم‌استانی عزیز 👋
 
